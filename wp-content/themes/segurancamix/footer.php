ec9e147cf4bb1d69cc52add437c2cdc6198996e9
Eduardo Sousa <webx_555@hotmail.com>
2023-11-09 16:38:07 -0300

    Feita as estruturas da home

--- wp-content/themes/segurancamix/footer.php
+++ wp-content/themes/segurancamix/footer.php
@@ -1,4 +1,53 @@
 <footer>
+    <section class="home-fale_conosco">
+        <p>Fale conosco, solicite orçamento.</p>
+        <button>
+            <a href="http://">SOLICITAR ORÇAMENTO</a>
+        </button>
+    </section>
+    <section class="info-rodape">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">lala</div>
+                <div class="col-md-3">
+                    <div class="header-contact__content">
+                        <a href="tel:1145911709" class="header-contact__item" target="_blank" rel="noopener">
+                            <i class="header-contact__icon icon fas fa-phone"></i>
+                            <span>(11) 0000-0000</span>
+                        </a>
+                        <a href="mailto:contato@segurancamix.com.br" class="header-contact__item" target="_blank" rel="noopener">
+                            <i class="header-contact__icon icon fas fa-envelope"></i>
+                            <span>contato@segurancamix.com.br</span>
+                        </a>
+                        <a href="mailto:contato@segurancamix.com.br" class="header-contact__item" target="_blank" rel="noopener">
+                            <i class="header-contact__icon icon fas fa-map-marker-alt"></i>
+                            <span>contato@segurancamix.com.br</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="header-contact__social social">
+                        <a href="" rel="noopener" target="_blank" class="social__item" title="Facebook">
+                            <i class="social__icon icon fab fa-facebook-f"></i>
+                        </a>
+                        <a href="" rel="noopener" target="_blank" class="social__item" title="Instagram">
+                            <i class="social__icon icon fab fa-instagram"></i>
+                        </a>
+                        <a href="" rel="noopener" target="_blank" class="social__item" title="LinkedIn">
+                            <i class="social__icon icon fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">lala</div>
+            </div>
+        </div>
+    </section>
+    <div class="footer__copyright">
+        <div class="container position-relative">
+            <span>© Plataformanet 2023 - Todos os direitos reservados</span>
+            <span class="politica-privacidade">Política de privacidade</span>
+        </div>
+    </div>
 </footer>
 </main>
 
--- wp-content/themes/segurancamix/home.php
+++ wp-content/themes/segurancamix/home.php
@@ -49,4 +49,100 @@ get_header();
     </div>
 </section>
 
+<section class="certificados">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Certificações</h2>
+                <h3>Somos certificados</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores. Minus magnam aliquid labore?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores.
+                </p>
+                <button class="btn-sobre_nos">
+                    <a href="http://">VER TODOS</a>
+                </button>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+</section>
+
+<section class="produtos">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Produtos</h2>
+                <h3>Nossos produtos</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores. Minus magnam aliquid labore?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores.
+                </p>
+                <button class="btn-sobre_nos">
+                    <a href="http://">SAIBA MAIS</a>
+                </button>
+            </div>
+            <div class="col-md-8"></div>
+        </div>
+    </div>
+</section>
+
+<section class="projetos">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Projetos</h2>
+                <h3>O que já produzimos</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores. Minus magnam aliquid labore?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="carrousel-projetos">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 projetos_comerciais">
+                    <h2>Projetos residenciais</h2>
+                </div>
+                <div class="col-md-6 projetos_residenciais">
+                    <h2>Projetos comerciais</h2>
+                </div>
+                <button class="btn-carrousel_projetos">
+                    <a href="http://">SAIBA MAIS</a>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="nossos-cliente">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Clientes</h2>
+                <h3>Nossos clientes</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores. Minus magnam aliquid labore?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="confira-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Blog</h2>
+                <h3>Confira nosso blog</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores. Minus magnam aliquid labore?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis doloremque ab, iure, inventore dicta, obcaecati labore explicabo nulla quasi natus dolores.
+                </p>
+            </div>
+            <button class="btn-configura_blog">
+                <a href="http://">ACESSAR BLOG</a>
+            </button>
+        </div>
+    </div>
+</section>
+
 <? get_footer(); ?>
